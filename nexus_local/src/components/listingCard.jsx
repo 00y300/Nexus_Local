45c2d4ecba4375858d165746f9fdd79dfa4326ef
00y300 <navajerry83@gmail.com>
2025-03-13 19:21:28 -0400

    Listing Card and TailwindCSS
    Added more functionality to the listing card. If there is no image
    source provided, then, the image card will render a fallback image.
    
    Also a the fallback image to the public folder in the code.
    
    Also added some detailed comments throught the channged files.

--- nexus_local/src/components/listingCard.jsx
+++ nexus_local/src/components/listingCard.jsx
@@ -3,13 +3,30 @@
 // The cards will diplay the item picture, small description
 // MAYBE: option to add cart
 
-const listingCard = () => {
-  // Some of the parameters should be the image, small desc., and price.
+import Image from "next/image";
+
+const ListingCard = ({ children, imgsrc, ...props }) => {
+  // Fallback image URL, e.g., a default image in your public folder
+  // Question-Mark
+  const fallbackimageSrc = "/Question-Mark.png";
+
   return (
-    <>
-      <h1>Sample Text for the listing card</h1>
-    </>
+    <div
+      {...props}
+      className="group relative h-80 max-w-xs overflow-hidden rounded-2xl shadow-lg"
+    >
+      {/* The image will a a render a default image a question mark if an image is not proide */}
+      <Image
+        fill={true}
+        src={imgsrc || fallbackimageSrc}
+        alt="Listing Image"
+        className="h-full w-full object-cover transition-transform duration-200 group-hover:scale-110"
+      />
+      <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent">
+        <div className="p-4 text-white">{children}</div>
+      </div>
+    </div>
   );
 };
 
-export default listingCard;
+export default ListingCard;
