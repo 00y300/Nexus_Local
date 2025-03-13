3ffd14d99fa8b3f732364008b57e8f1dd89158bc
00y300 <navajerry83@gmail.com>
2025-03-13 19:58:37 -0400

    Rename nexus_local to Frontend
    Rename nexus_local folder to frontned

--- frontend/src/pages/listing.jsx
+++ frontend/src/pages/listing.jsx
@@ -1,7 +1,11 @@
+import ListingCard from "@/components/listingCard";
+
 const ListingPage = () => {
   return (
     <div className="text-center">
       <h1>Here is the listing page</h1>
+
+      <ListingCard></ListingCard>
     </div>
   );
 };
